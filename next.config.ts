fa4540a577c23c8ad9843e67f8b19c4716a19ede
putra <marrufhariam@gmail.com>
2026-01-08 18:53:04 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',          // path di frontend
+        destination: 'http://76.13.16.58/:path*', // path di VPS backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
